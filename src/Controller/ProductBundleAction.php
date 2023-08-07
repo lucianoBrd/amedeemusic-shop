ac13d1b44f612d38d7bea667349112cc3905381b
lucianoBrd <lucien.burdet@gmail.com>
2023-08-07 12:07:58 +0200

    modify quantity of same variant when add to cart

--- src/Controller/ProductBundleAction.php
+++ src/Controller/ProductBundleAction.php
@@ -2,22 +2,24 @@
 
 namespace App\Controller;
 
+use App\Repository\ProductBundle\ProductBundleRepository;
 use Doctrine\Persistence\ObjectManager;
-use Symfony\Component\HttpFoundation\Request;
-use Symfony\Component\HttpFoundation\Response;
+use Sylius\Component\Channel\Context\ChannelNotFoundException;
+use Sylius\Component\Channel\Model\ChannelInterface;
+use Sylius\Component\Core\Context\ShopperContextInterface;
 use Sylius\Component\Core\Model\OrderInterface;
 use Sylius\Component\Core\Model\OrderItemInterface;
-use Symfony\Component\HttpFoundation\RedirectResponse;
-use Sylius\Component\Resource\Factory\FactoryInterface;
 use Sylius\Component\Core\Model\ProductVariantInterface;
-use Sylius\Component\Order\Context\CartContextInterface;
-use App\Repository\ProductBundle\ProductBundleRepository;
-use Sylius\Component\Core\Context\ShopperContextInterface;
-use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
-use Sylius\Bundle\ResourceBundle\Controller\ResourceController;
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Sylius\Component\Core\Repository\ProductVariantRepositoryInterface;
+use Sylius\Component\Order\Context\CartContextInterface;
+use Sylius\Component\Order\Context\CartNotFoundException;
 use Sylius\Component\Order\Modifier\OrderItemQuantityModifierInterface;
+use Sylius\Component\Resource\Factory\FactoryInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\RedirectResponse;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
 
 class ProductBundleAction extends AbstractController
 {
@@ -36,41 +38,79 @@ class ProductBundleAction extends AbstractController
         $bundleName = $request->attributes->get('name');
         $bundle = $productBundleRepository->findOneBy(['name' => $bundleName]);
 
-        /** @var OrderInterface $order */
         $order = $this->getCurrentCart();
+        $channel = $this->getCurrentChannel();
 
-        $channel = $this->shopperContext->getChannel();
-        foreach ($bundle->getProducts() as $product)
-        {
-            $productId = (int) $product->getVariants()->first()->getId();
-            /** @var ProductVariantInterface $variant */
-            $variant = $this->productVariantRepository->find($productId);
+        if ($order && $channel) {
+            $orderItems = $order->getItems();
+            foreach ($bundle->getProducts() as $product) {
+                $productId = (int) $product->getVariants()->first()->getId();
+                /** @var ProductVariantInterface $variant */
+                $variant = $this->productVariantRepository->find($productId);
 
-            /** @var OrderItemInterface $orderItem */
-            $orderItem = $this->orderItemFactory->createNew();
-            $variant->setShippingRequired(false);
-            $orderItem->setVariant($variant);
-            $price = $variant->getChannelPricingForChannel($channel)->getPrice();
-            $orderItem->setUnitPrice($price);
+                /** @var OrderItemInterface $orderItem */
+                $orderItem = $this->orderItemFactory->createNew();
+                $variant->setShippingRequired(false);
+                $orderItem->setVariant($variant);
+                $price = $variant->getChannelPricingForChannel($channel)->getPrice();
+                $orderItem->setUnitPrice($price);
 
-            $this->orderItemQuantityModifier->modify($orderItem, 1);
+                $targetQuantity = 1;
 
-            $order->addItem($orderItem);
+                foreach ($orderItems as $oi) {
+                    if ($oi->getVariant()->getId() == $productId) {
+                        $orderItem = $oi;
+                        $targetQuantity = $oi->getQuantity() + 1;
+                        break;
+                    }
+                }
+
+                $this->orderItemQuantityModifier->modify($orderItem, $targetQuantity);
+
+                if ($targetQuantity == 1) {
+                    $order->addItem($orderItem);
+                }
+            }
+
+            $order->setChannel($channel);
+            $order->setLocaleCode($this->shopperContext->getLocaleCode());
+            $order->setCurrencyCode($this->shopperContext->getCurrencyCode());
+
+            $this->orderManager->persist($order);
+            $this->orderManager->flush();
         }
-
-        $order->setChannel($channel);
-        $order->setLocaleCode($this->shopperContext->getLocaleCode());
-        $order->setCurrencyCode($this->shopperContext->getCurrencyCode());
-
-        $this->orderManager->persist($order);
-        $this->orderManager->flush();
 
         $cartPage = $this->router->generate('sylius_shop_cart_summary');
         return new RedirectResponse($cartPage);
     }
 
-    protected function getCurrentCart(): OrderInterface
+    private function getCurrentCart(): ?OrderInterface
     {
-        return $this->cartContext->getCart();
+        try {
+            $cart = $this->cartContext->getCart();
+        } catch (CartNotFoundException) {
+            return null;
+        }
+
+        if (!$cart instanceof OrderInterface) {
+            return null;
+        }
+
+        return $cart;
+    }
+
+    private function getCurrentChannel(): ?ChannelInterface
+    {
+        try {
+            $channel = $this->shopperContext->getChannel();
+        } catch (ChannelNotFoundException) {
+            return null;
+        }
+
+        if (!$channel instanceof ChannelInterface) {
+            return null;
+        }
+
+        return $channel;
     }
 }
