ea9b333fb74f524945f6092d9b973c2b3c1cfd48
lucianoBrd <lucien.burdet@gmail.com>
2023-08-28 17:00:25 +0200

    review wishlist template

--- themes/BootstrapChildTheme/assets/js/fontawesome.js
+++ themes/BootstrapChildTheme/assets/js/fontawesome.js
@@ -7,6 +7,11 @@ import { faTiktok } from '@fortawesome/free-brands-svg-icons/faTiktok';
 import { faBolt } from '@fortawesome/free-solid-svg-icons/faBolt';
 import { faHeart } from '@fortawesome/free-solid-svg-icons/faHeart';
 import { faList } from '@fortawesome/free-solid-svg-icons/faList';
+import { faRemove } from '@fortawesome/free-solid-svg-icons/faRemove';
+import { faCartShopping } from '@fortawesome/free-solid-svg-icons/faCartShopping';
+import { faCopy } from '@fortawesome/free-solid-svg-icons/faCopy';
+import { faFileExport } from '@fortawesome/free-solid-svg-icons/faFileExport';
+import { faEdit } from '@fortawesome/free-solid-svg-icons/faEdit';
 
-library.add(faTiktok, faAngleRight, faBolt, faHeart, faList);
+library.add(faTiktok, faAngleRight, faBolt, faHeart, faList, faRemove, faCartShopping, faCopy, faFileExport, faEdit);
 dom.watch();
